653a381234fb740b53fc88b111cd5d1778df22e3
Bradley Hammond <bradicaljh@hotmail.com>
2012-04-07 12:32:24 +1000

    Just made the sample script cleaner and more 'bare minimum'

--- bot/pIRCbot.pm
+++ bot/pIRCbot.pm
@@ -1,28 +1,11 @@
 #!/usr/bin/perl
-
-############################################################################
-## This file is part of pIRC.                                             ##
-##                                                                        ##
-## pIRC is free software: you can redistribute it and/or modify           ##
-## it under the terms of the GNU General Public License as published by   ##
-## the Free Software Foundation, either version 3 of the License, or      ##
-## (at your option) any later version.                                    ##
-##                                                                        ##
-## pIRC is distributed in the hope that it will be useful,                ##
-## but WITHOUT ANY WARRANTY; without even the implied warranty of         ##
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           ##
-## GNU General Public License for more details.                           ##
-##                                                                        ##
-## You should have received a copy of the GNU General Public License      ##
-## along with pIRC. If not, see <http://www.gnu.org/licenses/>.           ##
-############################################################################
-
-# Don't change these unless you're adding dependencies to the list
 package bot::pIRCbot;
 our @EXPORT = qw($host $port $nickname $username $autojoin);
 use Exporter qw(import);
 use strict;
 use warnings;
+
+# -- Only add your bot code bellow this line unless you need a dependency
 
 # Variables for server connection, change this to reflect your bot info
 our $host = 'irc.serenia.net';
@@ -31,46 +14,23 @@ our $nickname = 'pIRCbot';
 our $username = 'pIRCbot';
 our $autojoin = '';   # Leave blank for none
 
-
+# We got an invite
 sub GotInvite
 {
     my ($nick, $address, $channel) = @_;
-    # We got an invite!
-    # -
-    # This is an example of how we would handle an invite or another event
-    &::SocketSend("JOIN " . $channel);
-    &::SocketSend("PRIVMSG " . $channel . " Hello $nick!");
+    # Automatically accept any invite to channels
+    &::SocketSend("JOIN $channel");
 }
 
-sub GotJoin
-{
-    my ($nick, $address, $channel) = @_;
-    # Someone (or we) joined
-}
-
-sub GotPart
-{
-    my ($nick, $address, $channel, $reason) = @_;
-    # Someone (or we) left the channel
-}
-
+# We got a message in a channel!
 sub GotChannelMessage
 {
     my ($nick, $address, $channel, $message) = @_;
-    # We got a message in a channel!
+    # Respond to anyone saying 'ping' with 'pong'
+    if ($message eq 'ping')
+    {
+        &::SocketSend("PRIVMSG $channel pong");
+    }
 }
 
-sub GotPrivateMessage
-{
-    my ($nick, $address, $message) = @_;
-    # We got a message in private!
-}
-
-sub GotQuit
-{
-    my ($nick, $address, $channel, $reason) = @_;
-    # Someone quit
-}
-
-# Return true, do not change this
-1;
+1; # Return true, do not change this
